30406f7cf0c640ad2993d5274bf6f9e7c289183b
ApplyKCL <sh258955@dal.ca>
2024-12-29 11:52:34 -0400

    Chapter 3 Train 4 finished

---
+++ Chaper3/C3T4/C3T4/C3T4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace C3T4
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            string [] pass = new string [6];
+            
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Enter password: ");
+                pass[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Password {0}: {1}", i + 1, pass[i]);
+            }
+        }
+    }
+}
+
+// Text Book Answer
+// namespace Demo
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             string password = "";
+//             Console.Write("Enter the Password：");
+//             password += Console.ReadLine();
+//             Console.Write("Enter the Password：");
+//             password += "," + Console.ReadLine();
+//             Console.Write("Enter the Password：");
+//             password += "," + Console.ReadLine();
+//             Console.Write("Enter the Password：");
+//             password += "," + Console.ReadLine();
+//             Console.Write("Enter the Password：");
+//             password += "," + Console.ReadLine();
+//             Console.Write("Enter the Password：");
+//             password += "," + Console.ReadLine();
+//             Console.Write("\n The Password Your Entered in 6 Times is: "+password);
+//             Console.ReadLine();
+//         }
+//     }
+// }
