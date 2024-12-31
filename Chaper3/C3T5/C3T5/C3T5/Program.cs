ec00cba2eeaf613489975e81289bdddff163f145
ApplyKCL <sh258955@dal.ca>
2024-12-31 00:55:11 -0400

    The Chapter 3 Train 5 Finsihed

---
+++ Chaper3/C3T5/C3T5/C3T5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace C3T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float radius = 0f;
+            const float PI = 3.14159f;
+            float area = 0f;
+            Console.WriteLine("Enter the radius of the circle: ");
+            radius = float.Parse(Console.ReadLine());
+            area = PI * radius * radius;
+            Console.WriteLine("The area of the circle is: " + area);
+        }
+    }
+}
