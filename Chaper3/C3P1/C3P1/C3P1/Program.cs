25047085468aeee778ac2c86a6e9390926eef450
ApplyKCL <sh258955@dal.ca>
2025-01-01 16:54:08 -0400

    C3P5 Finished

---
+++ Chaper3/C3P1/C3P1/C3P1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C3P1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please Enter Your Home Address:");
+            string address = Console.ReadLine();
+            Console.WriteLine("Please Enter Your City:");
+            string city = Console.ReadLine();
+            Console.WriteLine("Please Enter Your State:");
+            string state = Console.ReadLine();
+            Console.WriteLine("Please Enter Your Zip Code:");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Please Enter Your Country:");
+            string country = Console.ReadLine();
+            Console.WriteLine("Your Address is: \n" + address + "\n" + city + ", " + state + " " + zip + "\n" + country);
+        }
+    }
+}
