3c4bbef487b722ad3edc3c2d56fea01948a5963d
ApplyKCL <sh258955@dal.ca>
2025-01-02 17:55:45 -0400

    Finish C3P4

---
+++ Chaper3/C3P4/C3P4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace C3P4
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double shouldPay = 100.00;
+            double actualPay = 0;
+            
+            Console.WriteLine("You should Pay: " + shouldPay);
+            Console.WriteLine("Please input the actual pay: ");
+            actualPay = Convert.ToDouble(Console.ReadLine());
+            if (actualPay > shouldPay)
+            {
+                Console.WriteLine("You should pay: " + shouldPay);
+                Console.WriteLine("You actually pay: " + actualPay);
+                Console.WriteLine("The change is: " + (actualPay - shouldPay));
+            }
+            else if(actualPay<shouldPay)
+            {
+                Console.WriteLine("You should pay: " + shouldPay);
+                Console.WriteLine("You actually pay: " + actualPay);
+                Console.WriteLine("You still need to pay: " + (shouldPay - actualPay));
+            }
+            else
+            {
+                Console.WriteLine("You pay the right amount");
+            }
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("Purchase Time\t" + dt);
+        }
+    }
+}
+
