0fbb16a21fcf489977613b07e44ed73a2ab115c4
ApplyKCL <sh258955@dal.ca>
2025-01-03 12:30:28 -0400

    C4T1 Finished

---
+++ C4/C4T1/C4T1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C4T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many Purchase has been made?");
+            int purchase = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Amount of Chicken Food Been Made {0} g", purchase * 180);
+        }
+    }
+}
