85ae5021f14d3e41d301031c356587864b1c3419
ApplyKCL <sh258955@dal.ca>
2025-01-03 11:47:57 -0400

    Add C4 Folder

---
+++ C3/C3P5/C3P5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C3P5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How Many Bags of Milks Do You Want?");
+            int bags = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Total Protain is: {0:f1}", bags * 6.4);
+        }
+
+    }
+}
--- C3/C3P5/C3P5/obj/Debug/net9.0/C3P5.AssemblyInfo.cs
+++ C3/C3P5/C3P5/obj/Debug/net9.0/C3P5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C3P5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e683a0743154e17d6f42aec254d303442e6e5627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4bbef487b722ad3edc3c2d56fea01948a5963d")]
 [assembly: System.Reflection.AssemblyProductAttribute("C3P5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C3P5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
