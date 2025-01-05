67e556c07062037257c528ce4e74251b30e3458c
ApplyKCL <sh258955@dal.ca>
2025-01-05 17:07:29 -0400

    Finished C4P4 & C4P5

--- C4/C4P4/C4P4/Program.cs
+++ C4/C4P4/C4P4/Program.cs
@@ -4,6 +4,11 @@ namespace C4P4
 {
     class Program
     {
-        
+        static void Main()
+        {
+            Console.WriteLine("How Many Energy Do you Have?");
+            int energy = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Total Award Amount is {0}", energy/100);
+        }
     }
 }
---
+++ C4/C4P5/C4P5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace C4P5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random rand = new Random();
+            int fieldOne = rand.Next(0, 10);
+            int fieldTwo = rand.Next(0, 10);
+            int token = rand.Next(0, 10);
+            int answer = 0;
+            Console.WriteLine("Please Enter the Correct Ans for the following Equation: ");
+            if((token & 1) == 0)
+            {
+                Console.WriteLine(fieldOne + " + " + fieldTwo + " = ?");
+                answer = fieldOne + fieldTwo;
+            }
+            else
+            {
+                Console.WriteLine(fieldOne + " * " + fieldTwo + " = ?");
+                answer = fieldOne * fieldTwo;
+            }
+            int userAnswer = Convert.ToInt32(Console.ReadLine());
+            if(userAnswer == answer)
+            {
+                Console.WriteLine("Correct Ans");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Ans");
+            }
+        }
+        
+    }
+}
