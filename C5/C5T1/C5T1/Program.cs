6f462cdab32f9db36a1a288560037dedc0dbb888
ApplyKCL <sh258955@dal.ca>
2025-01-05 20:54:03 -0400

    Finished C5T1

--- C5/C5T1/C5T1/Program.cs
+++ C5/C5T1/C5T1/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace C5T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tYour Insurance Info");
+            Console.WriteLine("Your Insurance Number: 123456789");
+            Console.WriteLine("Your Insurance Name: John Doe");
+            Console.WriteLine("Your Insurance Address: 1234 Elm St.");
+            Console.WriteLine("Please Enter Your Gender: ");
+            char gender = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Your Gender: {0}", gender);
+        }
+    }
+}
--- C5/C5T1/C5T1/obj/Debug/net9.0/C5T1.AssemblyInfo.cs
+++ C5/C5T1/C5T1/obj/Debug/net9.0/C5T1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C5T1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e556c07062037257c528ce4e74251b30e3458c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e258eb2b4898b360d8de7c4a981341b562bb0b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("C5T1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C5T1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
