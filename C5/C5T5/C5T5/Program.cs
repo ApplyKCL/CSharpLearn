f33c6fc417ee240ed0e56b03b2176c61cf8dc562
ApplyKCL <sh258955@dal.ca>
2025-01-05 22:31:57 -0400

    Finished Chapter 5 Trainning

---
+++ C5/C5T5/C5T5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace C5T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello, World!";
+            StringBuilder strbld = new StringBuilder(str);
+            Console.WriteLine(str);
+            Console.WriteLine(strbld);
+            
+            
+            Console.WriteLine(str.GetHashCode());
+            str = str.Insert(0, "Hello, World!");
+            Console.WriteLine(str.GetHashCode());
+            Console.WriteLine(strbld.GetHashCode());
+            strbld.Insert(0, "Hello, World!");
+            Console.WriteLine(strbld.GetHashCode());
+        }
+    }
+}
---
+++ C5/C5T6/C5T6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace C5T6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder strBld = new StringBuilder("Joe");
+            strBld.Insert(0, "Hello ");
+            Console.WriteLine(strBld);
+        }
+    }
+}
