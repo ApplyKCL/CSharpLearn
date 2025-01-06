13f77c1a0d878007eced3cfdccc5336082b3b310
ApplyKCL <sh258955@dal.ca>
2025-01-05 21:29:14 -0400

    Finished C5T3&T4

---
+++ C5/C5T3/C5T3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C5T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int index =  input.IndexOf("Dream");
+            if (index == -1)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+            
+        }
+    }
+}
---
+++ C5/C5T4/C5T4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace C5T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Product List******");
+            Console.WriteLine("1. Apple999X 1000\n2. Banana1000Pro 999.99\n3. Orange1500Ultra 1000.99");
+            Console.WriteLine("*************************");
+            Console.WriteLine("Enter the product number: ");
+            int productNumber = Convert.ToInt32(Console.ReadLine());
+            switch (productNumber)
+            {
+                case 1:
+                    Console.WriteLine("You Purchased Apple999X, which costs {0:C0}", 1000);
+                    break;
+                case 2:
+                    Console.WriteLine("You Purchased Banana1000Pro, which costs {0:C2}", 999.99);
+                    break;
+                case 3:
+                    Console.WriteLine("You Purchased Orange1500Ultra, which costs {0:C2}", 1000.99);
+                    break;
+                default:
+                    Console.WriteLine("Invalid product number");
+                    break;
+            }
+        }
+    }
+}
