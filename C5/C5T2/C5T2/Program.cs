a49576f54f661fbbf9ecb04ffa8f0f57f7d59f06
ApplyKCL <sh258955@dal.ca>
2025-01-05 20:57:04 -0400

    Finish C5T2

---
+++ C5/C5T2/C5T2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C5T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\code\\csharp\\CSharpLearn\\CSharpLearn\\C5\\C5T2"; // Double backslashes are used to escape the backslash character
+            Console.WriteLine("The System Path is of This File is: " + path);
+        }
+    }
+}
