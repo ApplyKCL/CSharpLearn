29dd6d5c1fc2a511c229e2095ab6eb854ce8e7e1
ApplyKCL <sh258955@dal.ca>
2025-01-07 09:49:45 -0400

    Finished C3P3

---
+++ C6/C6T3/C6T3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C6T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("Enter the Expected N");
+            n = Convert.ToInt32(Console.ReadLine());
+            int reuslt = 1;
+            int previous = 0;
+            while (n-1 > 0)
+            {
+                Console.WriteLine(reuslt);
+                reuslt += previous;
+                previous = reuslt - previous;
+                n--;
+            }
+            Console.WriteLine(reuslt);
+        }
+    }
+}
