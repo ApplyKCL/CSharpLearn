f867d012dd376189d91c8fa2b51cd6dbd080f434
ApplyKCL <sh258955@dal.ca>
2025-01-07 11:10:04 -0400

    C6T7 Finished

---
+++ C6/C6T7/C6T7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace C6T7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Number to Check if it is prime number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num == 0 || num == 1)
+            {
+                Console.WriteLine("Number is not prime");
+                return;
+            }
+            
+            for(int i = 2; i < num; i++)
+            {
+                if(num % i == 0)
+                {
+                    Console.WriteLine("Number is not prime");
+                    return;
+                }
+            }
+            Console.WriteLine("Number is prime");
+        }
+    }
+}
