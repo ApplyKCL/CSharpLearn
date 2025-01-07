8b38fbf663b45614163b1ae35cbad28dba177326
ApplyKCL <sh258955@dal.ca>
2025-01-07 15:50:51 -0400

    Finished C6P1

---
+++ C6/C6P1/C6P1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace C6P1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("This Program will calculate the result based on the following formula:");
+            Console.WriteLine("b=3a (a<50)");
+            Console.WriteLine("b=6a + 60 (50<=a<500)");
+            Console.WriteLine("b=9a - 90 (a>=500)");
+            Console.WriteLine("Please Enter the number a");
+            int a = int.Parse(Console.ReadLine());
+            int b = 0;
+            if(a < 50)
+            {
+                b = 3 * a;
+            }
+            else if(a >= 50 && a < 500)
+            {
+                b = 6 * a + 60;
+            }
+            else
+            {
+                b = 9 * a - 90;
+            }
+            Console.WriteLine("The result is: " + b);
+        }
+    }
+}
