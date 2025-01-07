635313d9f5afcfaef5b65a1629ce5cea20937727
ApplyKCL <sh258955@dal.ca>
2025-01-07 16:18:21 -0400

    C6P3 Unfinished

---
+++ C6/C6P3/C6P3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C6P3
+{
+    class Program
+    {
+        static void Main()
+        { 
+            Console.WriteLine("Define the Max Length of the Title that you want to display: ");
+            int maxLength = Convert.ToInt32(Console.ReadLine());
+            string title = "Please Enter a Title";
+            while (!title.Equals("END"))
+            {
+                
+            }
+
+        }
+    }
+}
