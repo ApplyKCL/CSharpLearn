487cbc75b2686a6e1e3012ef8630dcf7473d5cad
ApplyKCL <sh258955@dal.ca>
2025-01-06 21:16:03 -0400

    Finished C5 P1 & P2, and Still have P3 - P5 unfinished

---
+++ C5/C5P1/C5P1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C5P1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a sentence that u want to reverse: ");
+            string name = Console.ReadLine();
+            
+            for(int i=name.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(name[i]);
+            }
+            
+        }
+    }
+}
---
+++ C5/C5P2/C5P2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C5P2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the String that you want to remove the Spaces from:");
+            string input = Console.ReadLine();
+            string [] words = input.Split(' ');
+            string output = "";
+            foreach (string word in words)
+            {
+                output += word;
+            }
+            Console.WriteLine("The String after removing the Spaces is: " + output);
+        }
+    }
+}
