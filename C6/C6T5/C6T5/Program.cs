4f4480416030b54f6eb1d67c3c4317a505ed0012
ApplyKCL <sh258955@dal.ca>
2025-01-07 10:50:24 -0400

    C6T5 Finished

---
+++ C6/C6T5/C6T5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace C6T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Height of the Triangle: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The Elements of the Triangle: ");
+            string element = Console.ReadLine();
+            int heightIndex = 0;
+            int widthIndex = 0;
+            while (heightIndex < height)
+            {
+                for(int i = 0; i < height - heightIndex; i++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 0; j < heightIndex*2+1; j++)
+                {
+                    Console.Write(element);
+                }
+                Console.WriteLine();
+                heightIndex++;
+            }
+        }
+    }
+}
