4cc1000ff8187f6509e2beaf9a59d8b2bf5f9154
ApplyKCL <sh258955@dal.ca>
2025-01-07 10:36:03 -0400

    C6T4 Finished

---
+++ C6/C6T4/C6T4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C6T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int guess = random.Next(1, 200);
+            int input = 0;
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 200");
+                input = int.Parse(Console.ReadLine());
+                if (input > guess)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else if (input < guess)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else
+                {
+                    Console.WriteLine("Correct");
+                }
+            }while(input != guess);
+        }
+    }
+};
+
