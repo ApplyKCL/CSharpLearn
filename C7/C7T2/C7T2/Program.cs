86c3b4e47d2b5bf63d79db0792100d6af9c32682
ApplyKCL <sh258955@dal.ca>
2025-01-07 21:51:13 -0400

    C7T2

---
+++ C7/C7T2/C7T2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C7T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] cards =  GenerateCards();
+            Random random = new Random();
+            int handCardCount = 0;
+            while (handCardCount < 52)
+            {
+                int card = random.Next(0, 13);
+                if (cards[card] > 0)
+                {
+                    cards[card]--;
+                    handCardCount++;
+                    Console.WriteLine("You got a card {0}: remind {1}", card, cards[card]);
+                }
+            }
+
+        }
+        static int [] GenerateCards()
+        {
+            int [] cards = new int[13];
+            for(int i = 0; i < cards.Length; i++)
+            {
+                cards[i] = 4;
+            }
+            return cards;
+        }
+    }
+}
+
