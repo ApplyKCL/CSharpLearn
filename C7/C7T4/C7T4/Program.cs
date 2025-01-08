fe66b4c7fd4a4a1258ddf9a8fdfc372dee808543
ApplyKCL <sh258955@dal.ca>
2025-01-07 22:07:01 -0400

    C7T4

---
+++ C7/C7T4/C7T4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C7T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] info =
+            {
+                {"Yam", "1", "68.9"},
+                {"Potato", "2", "34.5"},
+                {"Tomato", "3", "45.6"},
+                {"Onion", "4", "23.4"},
+                {"Carrot", "5", "67.8"}
+            };
+            Print(info);
+        }
+        
+        static void Print(string[,] info)
+        {
+            
+            for (int i = 0; i < info.GetLength(0); i++)
+            {
+                string element = "";
+                for (int j = 0; j < info.GetLength(1); j++)
+                {
+                    element =element + info[i, j] + "\t";
+                }
+                Console.WriteLine(element);
+                Console.WriteLine();
+            }
+        }
+    }
+}
