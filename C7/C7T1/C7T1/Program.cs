0f2e547d4c6cbaae578f17faa751216d06f561e3
ApplyKCL <sh258955@dal.ca>
2025-01-07 21:42:34 -0400

    C7T1

---
+++ C7/C7T1/C7T1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C7T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentGrades = { 80, 90, 99, 60, 70 };
+            foreach (int studentGrade in studentGrades)
+            {
+                Console.WriteLine(studentGrade);
+            }
+            
+        }
+    }
+}
