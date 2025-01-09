253832d67c78b75217fb5195890ba6f8694ff5e8
ApplyKCL <sh258955@dal.ca>
2025-01-08 20:33:38 -0400

    C7T5

---
+++ C7/C7T5/C7T5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C7T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int [3, 3];
+            int a = 2;
+            int b = 1;
+            for (int i = 1; i <= 9; i++)
+            {
+                arr[a++, b++] = i;
+                
+                if (i % 3 == 0)
+                {
+                    a = a - 2;
+                    b = b - 1;
+                }
+                else
+                {
+                    a = a % 3;
+                    b = b % 3;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    Console.Write(arr[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
