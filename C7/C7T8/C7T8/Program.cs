bda69120a043a465325583e483ee973bc7ddc89b
ApplyKCL <sh258955@dal.ca>
2025-01-08 21:44:32 -0400

    C7T8

---
+++ C7/C7T8/C7T8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace C7T8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int [] arr = new int [10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(1, 100);
+            }
+            Console.WriteLine("Array before sorting:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            arr = selectionSort(arr);
+            Console.WriteLine("Array after sorting:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            
+        }
+        static int [] selectionSort(int [] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] > arr[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+                int temp = arr[maxIndex];
+                arr[maxIndex] = arr[i];
+                arr[i] = temp;
+            }
+            
+            
+            return arr;
+        }
+    }
+}
