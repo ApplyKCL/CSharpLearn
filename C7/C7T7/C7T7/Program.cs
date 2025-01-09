0c3c22f7382a3bbce0f8c2b56d0b8c1a85816a33
ApplyKCL <sh258955@dal.ca>
2025-01-08 21:31:55 -0400

    C7T7

---
+++ C7/C7T7/C7T7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace C7T7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[10];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 100);
+            }
+            numbers = PopSort(numbers);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        static int [] PopSort(int [] numbers)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                for(int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if(numbers[j] < numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            return numbers;
+        }
+    }
+}
