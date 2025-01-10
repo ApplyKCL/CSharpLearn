8186906e4437e00810414cfad122a7660dd2619e
ApplyKCL <sh258955@dal.ca>
2025-01-09 21:35:12 -0400

    C7T9

--- C7/C7T9/C7T9/Program.cs
+++ C7/C7T9/C7T9/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections;
 
-Console.WriteLine("Hello, World!");
+namespace C7T9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[]
+            {
+                "Ming", "Male", "1980-01-01", "Lee", "Female", "1981-01-01", "Mei", "Female", "1982-01-01"
+            };
+            ArrayList studentList = new ArrayList(students);
+            int index = 0;
+            foreach (string element in studentList)
+            {
+                Console.Write(element + "\t");
+                index++;
+                if (index % 3 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
--- C7/C7T9/C7T9/obj/Debug/net9.0/C7T9.AssemblyInfo.cs
+++ C7/C7T9/C7T9/obj/Debug/net9.0/C7T9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C7T9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda69120a043a465325583e483ee973bc7ddc89b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d7125e47d1107566c37ae4a8b8ac36c415c5ccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("C7T9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C7T9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
