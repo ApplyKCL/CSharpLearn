9dc425db03512f5af5a17eff16a9734c85de3b26
ApplyKCL <sh258955@dal.ca>
2025-01-09 21:36:30 -0400

    Copy File From T10

---
+++ C7/C7T10/C7T10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace C7T9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[]
+            {
+                "Ming", "Male", "1980-01-01", "Lee", "Female", "1981-01-01", "Mei", "Female", "1982-01-01"
+            };
+            ArrayList studentList = new ArrayList(students);
+            int index = 0;
+            foreach (string element in studentList)
+            {
+                Console.Write(element + "\t");
+                index++;
+                if (index % 3 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
