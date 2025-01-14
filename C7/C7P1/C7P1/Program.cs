1e574dfeb0b8738d05ae6ea42bba8d3d2107e479
ApplyKCL <sh258955@dal.ca>
2025-01-13 22:00:28 -0400

    C7P1

---
+++ C7/C7P1/C7P1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace C7P1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random rand = new Random();
+            int n = 10;
+            int [] arr = new int [n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rand.Next(1, 100);
+            }
+            Console.WriteLine("Before sorting:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            arr = shellSort(arr);
+            Console.WriteLine("After sorting:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            
+        }
+        static int [] shellSort(int [] arr)
+        {
+            int n = arr.Length;
+            for (int gap = n / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < n; i++)
+                {
+                    int temp = arr[i];
+                    int j;
+                    for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    {
+                        arr[j] = arr[j - gap];
+                    }
+                    arr[j] = temp;
+                }
+            }
+            return arr;
+        }
+    }
+}
