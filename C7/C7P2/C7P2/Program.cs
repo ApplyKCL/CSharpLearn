e73e1e4e5ee62b920703fff6004bd535e9991e4c
ApplyKCL <sh258955@dal.ca>
2025-01-13 22:09:03 -0400

    C7P2

---
+++ C7/C7P2/C7P2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace C7P2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a String");
+            string input = Console.ReadLine();
+            char [] charArry = input.ToCharArray();
+            
+            ArrayList list = new ArrayList(charArry);
+            foreach (char word in list)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine(list.Count);
+            
+        }
+    }
+}
