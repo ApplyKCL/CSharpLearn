112794ac0164cbe664a8da8e82a65c00e6cec822
ApplyKCL <sh258955@dal.ca>
2025-01-15 22:12:21 -0400

    C8T4

---
+++ C8/C8T4/C8T4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace C8T4
+{
+    class Program
+    {
+        class Numbers
+        {
+            private string[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+            public string getNumber(string number)
+            {
+                foreach (string num in numbers)
+                {
+                    if (num == number)
+                    {
+                        return num;
+                    }
+                }
+                return null;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+            Console.WriteLine("Enter a number: ");
+            string number = Console.ReadLine();
+            for(int i = 0; i < number.Length; i++)
+            {
+                string num = numbers.getNumber(number[i].ToString());
+                if (num == null)
+                {
+                    Console.WriteLine("Invalid number");
+                    return;
+                }else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            
+        }
+        
+    }
+}
